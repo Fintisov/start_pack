10858eadbfd14eec02b1419d56c36fbe1ae9a7f5
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-11-01 00:29:33 +0200

    create section "tabs" for settings-page

--- _src/scripts/script.js
+++ _src/scripts/script.js
@@ -1,5 +1,6 @@
 import modal from "./module/modal";
+import tabs from "./module/tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
-    modal(".my-modal", ".modal__btn-close", "#my-modal");
+
 })
